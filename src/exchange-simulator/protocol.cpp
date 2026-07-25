17ba0cf7b6c6d6a10364e03714d2fbda90be0ec6
Aditya Gupta <97134306+aaggupta07@users.noreply.github.com>
2026-07-25 16:27:42 -0500

    Bugfix message encoder

--- src/exchange-simulator/protocol.cpp
+++ src/exchange-simulator/protocol.cpp
@@ -5,8 +5,8 @@ namespace exchange {
 void MessageHeader::serialize(std::span<Byte> buffer) const {
     assert(buffer.size() >= sizeof(MessageHeader));
     
-    SequenceID network_sequence_number = ntohll(sequence_number);
-    Length network_payload_length = ntohs(payload_length);
+    SequenceID network_sequence_number = htonll(sequence_number);
+    Length network_payload_length = htons(payload_length);
     std::memcpy(buffer.data(), &network_sequence_number, sizeof(sequence_number));
     std::memcpy(buffer.data() + sizeof(network_payload_length), &network_payload_length, sizeof(payload_length));
 } 
